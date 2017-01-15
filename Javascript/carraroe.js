45cf5ea0b63c94aef030d2b7e1585b28a7e8e87d
davissandefur <davis.sandefur@gmail.com>
2017-01-15 16:31:56 -0600

    First commit

---
+++ Javascript/carraroe.js
@@ -0,0 +1,256 @@
+const doubleLSingleL = {
+  sound1: '/l̠ʲ/',
+  word1: 'buille',
+  source1: 'Carraroe/buille.ogg',
+  sound2: '/lʲ/ ',
+  word2:'buile',
+  source2:'Carraroe/buile.ogg'
+};
+
+const doubleNSingleN = {
+  sound1: '/n̠ʲ/',
+  word1: 'cinneadh',
+  source1: 'Carraroe/cinneadh.ogg',
+  sound2: '/nʲ/',
+  word2: 'cine',
+  source2: 'Carraroe/cine.ogg'
+}
+
+const broadPSlenderP = {
+  sound1: '/pˠ/',
+  word1: 'scóp',
+  source1: 'Carraroe/scóp.ogg',
+  sound2: '/pʲ/',
+  word2: 'scóip',
+  source2: 'Carraroe/scóip.ogg'
+}
+
+const broadBSlenderB = {
+  sound1: '/bˠ/',
+  word1: 'buí',
+  source1: 'Carraroe/buí.ogg',
+  sound2: '/bʲ/',
+  word2: 'bí',
+  source2: 'Carraroe/bí.ogg'
+}
+
+const broadTSlenderT = {
+  sound1: '/t̪ˠ/',
+  word1: 'tuí',
+  source1: 'Carraroe/tuí.ogg',
+  sound2: '/t̠ʲ/',
+  word2: 'tí',
+  source2: 'Carraroe/tí.ogg'
+}
+
+const broadDSlenderD = {
+  sound1: '/d̪ˠ/',
+  word1: 'dó',
+  source1: 'Carraroe/dó.ogg',
+  sound2: '/d̠ʲ/',
+  word2: 'deo',
+  source2: 'Carraroe/deo.ogg'
+}
+
+const broadCSlenderC = {
+  sound1: '/k/',
+  word1: 'cúis',
+  source1: 'Carraroe/cúis.ogg',
+  sound2: '/c/',
+  word2: 'ciumhais',
+  source2: 'Carraroe/ciumhais.ogg'
+}
+
+const broadGSlenderG = {
+  sound1: '/ɡ/',
+  word1: "(gal)",
+  source1: "Carraroe/gal.ogg",
+  sound2: '/ɟ/',
+  word2: '(geal)',
+  source2: "Carraroe/geal.ogg"
+}
+
+const broadFSlenderF = {
+  sound1: '/fˠ/',
+  word1: "(faoi)",
+  source1: 'Carraroe/faoi.ogg',
+  sound2: "/fʲ/",
+  word2:  "(figh)",
+  source2: "Carraroe/figh.ogg"
+}
+
+const broadBhSlenderBh = {
+  sound1: '/w/',
+  word1: 'bhuí',
+  source1: 'Carraroe/bhuí.ogg',
+  sound2: '/vʲ/',
+  word2: 'bhí',
+  source2: 'Carraroe/bhí.ogg'
+}
+
+const broadSSlenderS = {
+  sound1: '/s/',
+  word1: 'suí',
+  source1: 'Carraroe/suí.ogg',
+  sound2: '/ɕ/',
+  word2: 'sí',
+  source2: 'Carraroe/sí.ogg'
+}
+
+const broadChSlenderCh = {
+  sound1: '/x/',
+  word1: 'an chú',
+  source1: 'Carraroe/anchú.ogg',
+  sound2: '/ç/',
+  word2: 'an chiú',
+  source2: 'Carraroe/anchiú.ogg'
+}
+
+const broadGhSlenderGh = {
+  sound1: '/ɣ/',
+  word1: '(an-ghal)',
+  source1: 'Carraroe/an-ghal.ogg',
+  sound2: '/ʝ/',
+  word2: '(an-gheal)',
+  source2: 'Carraroe/an-gheal.ogg'
+}
+
+const broadMSlenderM = {
+  sound1: '/mˠ/',
+  word1: 'óm',
+  source1: 'Carraroe/óm.ogg',
+  sound2: '/mʲ/',
+  word2: 'óim',
+  source2: 'Carraroe/óim.ogg'
+}
+
+const broadNSlenderN = {
+  sound1: '/n̪ˠ/',
+  word1: '(naoi)',
+  source1: 'Carraroe/naoi.ogg',
+  sound2: '/n̠ʲ/',
+  word2: '(nigh)',
+  source2: 'Carraroe/nigh.ogg'
+}
+
+const broadNgSlenderNg = {
+  sound1: '/ŋ/',
+  word1: 'longa',
+  source1: 'Carraroe/longa.ogg',
+  sound2: '/ɲ/',
+  word2: 'loinge',
+  source2: 'Carraroe/loinge.ogg'
+}
+
+const broadRSlenderR = {
+  sound1: '/ɾˠ/',
+  word1: 'cur',
+  source1: 'Carraroe/cur.ogg',
+  sound2: '/ɾʲ/',
+  word2: 'cuir',
+  source2: 'Carraroe/cuir.ogg'
+}
+
+const broadLSlenderL = {
+  sound1: '/l̪ˠ/',
+  word1: 'lón',
+  source1: 'Carraroe/lon.ogg',
+  sound2: '/l̠ʲ/',
+  word2: 'leon',
+  source2: 'Carraroe/leon.ogg'
+}
+
+const broadPBroadB = {
+  sound1: '/pˠ/',
+  word1: 'pá',
+  source1: 'Carraroe/pa.ogg',
+  sound2: '/bˠ/',
+  word2: 'bá',
+  source2: 'Carraroe/ba.ogg'
+}
+
+const slenderPSlenderB = {
+  sound1: '/pʲ/',
+  word1: 'pinn',
+  source1: "Carraroe/pinn.ogg",
+  sound2: '/bʲ/',
+  word2: 'binn',
+  source2: "Carraroe/binn.ogg"
+}
+
+const broadTBroadD = {
+  sound1: '/t̪ˠ/',
+  word1: 'ghreadfadh',
+  source1: 'Carraroe/ghreadfadh.ogg',
+  sound2: '/d̪ˠ/',
+  word2: 'ghreadadh',
+  source2: 'Carraroe/ghreadadh.ogg'
+}
+
+const slenderTSlenderD = {
+  sound1: '/t̠ʲ/',
+  word1: 'chreidfeadh',
+  source1: 'Carraroe/chreidfeadh.ogg',
+  sound2: '/d̠ʲ/',
+  word2: 'chreideadh',
+  source2: 'Carraroe/chreideadh.ogg'
+}
+
+const broadCBroadG = {
+  sound1: '/k/',
+  word1: 'cá',
+  source1: 'Carraroe/cá.ogg',
+  sound2: '/g/',
+  word2: 'gá',
+  source2: 'Carraroe/gá.ogg'
+}
+
+const slenderCSlenderG = {
+  sound1: '/c/',
+  word1: 'ciall',
+  source1: 'Carraroe/ciall.ogg',
+  sound2: '/ɟ/',
+  word2: 'giall',
+  source2: 'Carraroe/giall.ogg'
+}
+
+const broadFandV = {
+  sound1: '/fˠ/',
+  word1: 'fóta',
+  source1: 'Carraroe/fóta.ogg',
+  sound2: '/v/',
+  word2: 'vóta',
+  source2: 'Carraroe/vóta.ogg'
+}
+
+const slenderFandV = {
+  sound1: "/fʲ/",
+  word1:  "(figh)",
+  source1: "Carraroe/figh.ogg",
+  sound2: "/vʲ/",
+  word2: 'bhí',
+  source2: 'Carraroe/bhí.ogg'
+}
+
+const broadChBroadGh = {
+  sound1: '/x/',
+  word1: 'an-chaol',
+  source1: 'Carraroe/an-chaol.ogg',
+  sound2: '/ɣ/',
+  word2: 'an-ghaol',
+  source2: 'Carraroe/an-ghaol.ogg'
+}
+
+const slenderChSlenderGh = {
+  sound1: '/ç/',
+  word1: 'cheal',
+  source1: 'Carraroe/cheal.ogg',
+  sound2: '/ʝ/',
+  word2: 'gheal',
+  source2: 'Carraroe/gheal.ogg'
+}
+
+const carraroeSounds=[broadPSlenderP, broadBSlenderB, broadTSlenderT, broadDSlenderD,
+broadCSlenderC,broadGSlenderG,broadFSlenderF,broadBhSlenderBh,broadSSlenderS,broadChSlenderCh,
+broadGhSlenderGh,broadMSlenderM,broadNSlenderN,broadNgSlenderNg,broadRSlenderR,broadLSlenderL,broadPBroadB,
+slenderPSlenderB,broadTBroadD,slenderTSlenderD,broadCBroadG,slenderCSlenderG,slenderFandV,broadChBroadGh,slenderChSlenderGh]
---
+++ Javascript/script.js
@@ -0,0 +1,13 @@
+const table = document.getElementById("carraroe");
+
+function addTableRows(element){
+  const  row = table.insertRow(-1);
+  const cell1 = row.insertCell(0);
+  const cell2 = row.insertCell(1);
+  cell1.innerHTML = `${element.sound1} (${element.word1}) <audio id='${element.word1}'
+  src=${element.source1}></audio><button onclick=document.getElementById('${element.word1}').play()>&#9658;</button>`;
+  cell2.innerHTML = `${element.sound2} (${element.word2}) <audio id='${element.word2}'
+  src=${element.source2}></audio><button onclick=document.getElementById('${element.word2}').play()>&#9658;</button>`;
+}
+
+carraroeSounds.forEach(addTableRows);
