47a51c2fd5b68a77f02dd0e3e69ec202261c485a
davissandefur <davis.sandefur@gmail.com>
2017-02-06 22:36:50 -0600

    Updated index.html and the javascript to handle multiple ones without delay on revealing table header and loading table rows. Also reorganized structure for audio files to make it more straightforward and easier to expand later.

--- Javascript/carraroe.js
+++ Javascript/carraroe.js
@@ -1,264 +1,260 @@
-(function(){
-const doubleLSingleL = {
-  sound1: '/l̠ʲ/',
-  word1: 'buille',
-  source1: 'Carraroe/buille.ogg',
-  sound2: '/lʲ/ ',
-  word2:'buile',
-  source2:'Carraroe/buile.ogg'
-};
+sounds = (function() {
+    const doubleLSingleL = {
+        sound1: '/l̠ʲ/',
+        word1: 'buille',
+        source1: 'Audio/Irish/Carraroe/buille.ogg',
+        sound2: '/lʲ/ ',
+        word2: 'buile',
+        source2: 'Audio/Irish/Carraroe/buile.ogg'
+    };
 
-const doubleNSingleN = {
-  sound1: '/n̠ʲ/',
-  word1: 'cinneadh',
-  source1: 'Carraroe/cinneadh.ogg',
-  sound2: '/nʲ/',
-  word2: 'cine',
-  source2: 'Carraroe/cine.ogg'
-}
+    const doubleNSingleN = {
+        sound1: '/n̠ʲ/',
+        word1: 'cinneadh',
+        source1: 'Audio/Irish/Carraroe/cinneadh.ogg',
+        sound2: '/nʲ/',
+        word2: 'cine',
+        source2: 'Audio/Irish/Carraroe/cine.ogg'
+    }
 
-const broadPSlenderP = {
-  sound1: '/pˠ/',
-  word1: 'scóp',
-  source1: 'Carraroe/scóp.ogg',
-  sound2: '/pʲ/',
-  word2: 'scóip',
-  source2: 'Carraroe/scóip.ogg'
-}
+    const broadPSlenderP = {
+        sound1: '/pˠ/',
+        word1: 'scóp',
+        source1: 'Audio/Irish/Carraroe/scóp.ogg',
+        sound2: '/pʲ/',
+        word2: 'scóip',
+        source2: 'Audio/Irish/Carraroe/scóip.ogg'
+    }
 
-const broadBSlenderB = {
-  sound1: '/bˠ/',
-  word1: 'buí',
-  source1: 'Carraroe/buí.ogg',
-  sound2: '/bʲ/',
-  word2: 'bí',
-  source2: 'Carraroe/bí.ogg'
-}
+    const broadBSlenderB = {
+        sound1: '/bˠ/',
+        word1: 'buí',
+        source1: 'Audio/Irish/Carraroe/buí.ogg',
+        sound2: '/bʲ/',
+        word2: 'bí',
+        source2: 'Audio/Irish/Carraroe/bí.ogg'
+    }
 
-const broadTSlenderT = {
-  sound1: '/t̪ˠ/',
-  word1: 'tuí',
-  source1: 'Carraroe/tuí.ogg',
-  sound2: '/t̠ʲ/',
-  word2: 'tí',
-  source2: 'Carraroe/tí.ogg'
-}
+    const broadTSlenderT = {
+        sound1: '/t̪ˠ/',
+        word1: 'tuí',
+        source1: 'Audio/Irish/Carraroe/tuí.ogg',
+        sound2: '/t̠ʲ/',
+        word2: 'tí',
+        source2: 'Audio/Irish/Carraroe/tí.ogg'
+    }
 
-const broadDSlenderD = {
-  sound1: '/d̪ˠ/',
-  word1: 'dó',
-  source1: 'Carraroe/dó.ogg',
-  sound2: '/d̠ʲ/',
-  word2: 'deo',
-  source2: 'Carraroe/deo.ogg'
-}
+    const broadDSlenderD = {
+        sound1: '/d̪ˠ/',
+        word1: 'dó',
+        source1: 'Audio/Irish/Carraroe/dó.ogg',
+        sound2: '/d̠ʲ/',
+        word2: 'deo',
+        source2: 'Audio/Irish/Carraroe/deo.ogg'
+    }
 
-const broadCSlenderC = {
-  sound1: '/k/',
-  word1: 'cúis',
-  source1: 'Carraroe/cúis.ogg',
-  sound2: '/c/',
-  word2: 'ciumhais',
-  source2: 'Carraroe/ciumhais.ogg'
-}
+    const broadCSlenderC = {
+        sound1: '/k/',
+        word1: 'cúis',
+        source1: 'Audio/Irish/Carraroe/cúis.ogg',
+        sound2: '/c/',
+        word2: 'ciumhais',
+        source2: 'Audio/Irish/Carraroe/ciumhais.ogg'
+    }
 
-const broadGSlenderG = {
-  sound1: '/ɡ/',
-  word1: "(gal)",
-  source1: "Carraroe/gal.ogg",
-  sound2: '/ɟ/',
-  word2: '(geal)',
-  source2: "Carraroe/geal.ogg"
-}
+    const broadGSlenderG = {
+        sound1: '/ɡ/',
+        word1: "(gal)",
+        source1: "Audio/Irish/Carraroe/gal.ogg",
+        sound2: '/ɟ/',
+        word2: '(geal)',
+        source2: "Audio/Irish/Carraroe/geal.ogg"
+    }
 
-const broadFSlenderF = {
-  sound1: '/fˠ/',
-  word1: "(faoi)",
-  source1: 'Carraroe/faoi.ogg',
-  sound2: "/fʲ/",
-  word2:  "(figh)",
-  source2: "Carraroe/figh.ogg"
-}
+    const broadFSlenderF = {
+        sound1: '/fˠ/',
+        word1: "(faoi)",
+        source1: 'Audio/Irish/Carraroe/faoi.ogg',
+        sound2: "/fʲ/",
+        word2: "(figh)",
+        source2: "Audio/Irish/Carraroe/figh.ogg"
+    }
 
-const broadBhSlenderBh = {
-  sound1: '/w/',
-  word1: 'bhuí',
-  source1: 'Carraroe/bhuí.ogg',
-  sound2: '/vʲ/',
-  word2: 'bhí',
-  source2: 'Carraroe/bhí.ogg'
-}
+    const broadBhSlenderBh = {
+        sound1: '/w/',
+        word1: 'bhuí',
+        source1: 'Audio/Irish/Carraroe/bhuí.ogg',
+        sound2: '/vʲ/',
+        word2: 'bhí',
+        source2: 'Audio/Irish/Carraroe/bhí.ogg'
+    }
 
-const broadSSlenderS = {
-  sound1: '/s/',
-  word1: 'suí',
-  source1: 'Carraroe/suí.ogg',
-  sound2: '/ɕ/',
-  word2: 'sí',
-  source2: 'Carraroe/sí.ogg'
-}
+    const broadSSlenderS = {
+        sound1: '/s/',
+        word1: 'suí',
+        source1: 'Audio/Irish/Carraroe/suí.ogg',
+        sound2: '/ɕ/',
+        word2: 'sí',
+        source2: 'Audio/Irish/Carraroe/sí.ogg'
+    }
 
-const broadChSlenderCh = {
-  sound1: '/x/',
-  word1: 'an chú',
-  source1: 'Carraroe/anchú.ogg',
-  sound2: '/ç/',
-  word2: 'an chiú',
-  source2: 'Carraroe/anchiú.ogg'
-}
+    const broadChSlenderCh = {
+        sound1: '/x/',
+        word1: 'an chú',
+        source1: 'Audio/Irish/Carraroe/anchú.ogg',
+        sound2: '/ç/',
+        word2: 'an chiú',
+        source2: 'Audio/Irish/Carraroe/anchiú.ogg'
+    }
 
-const broadGhSlenderGh = {
-  sound1: '/ɣ/',
-  word1: '(an-ghal)',
-  source1: 'Carraroe/an-ghal.ogg',
-  sound2: '/ʝ/',
-  word2: '(an-gheal)',
-  source2: 'Carraroe/an-gheal.ogg'
-}
+    const broadGhSlenderGh = {
+        sound1: '/ɣ/',
+        word1: '(an-ghal)',
+        source1: 'Audio/Irish/Carraroe/an-ghal.ogg',
+        sound2: '/ʝ/',
+        word2: '(an-gheal)',
+        source2: 'Audio/Irish/Carraroe/an-gheal.ogg'
+    }
 
-const broadMSlenderM = {
-  sound1: '/mˠ/',
-  word1: 'óm',
-  source1: 'Carraroe/óm.ogg',
-  sound2: '/mʲ/',
-  word2: 'óim',
-  source2: 'Carraroe/óim.ogg'
-}
+    const broadMSlenderM = {
+        sound1: '/mˠ/',
+        word1: 'óm',
+        source1: 'Audio/Irish/Carraroe/óm.ogg',
+        sound2: '/mʲ/',
+        word2: 'óim',
+        source2: 'Audio/Irish/Carraroe/óim.ogg'
+    }
 
-const broadNSlenderN = {
-  sound1: '/n̪ˠ/',
-  word1: '(naoi)',
-  source1: 'Carraroe/naoi.ogg',
-  sound2: '/n̠ʲ/',
-  word2: '(nigh)',
-  source2: 'Carraroe/nigh.ogg'
-}
+    const broadNSlenderN = {
+        sound1: '/n̪ˠ/',
+        word1: '(naoi)',
+        source1: 'Audio/Irish/Carraroe/naoi.ogg',
+        sound2: '/n̠ʲ/',
+        word2: '(nigh)',
+        source2: 'Audio/Irish/Carraroe/nigh.ogg'
+    }
 
-const broadNgSlenderNg = {
-  sound1: '/ŋ/',
-  word1: 'longa',
-  source1: 'Carraroe/longa.ogg',
-  sound2: '/ɲ/',
-  word2: 'loinge',
-  source2: 'Carraroe/loinge.ogg'
-}
+    const broadNgSlenderNg = {
+        sound1: '/ŋ/',
+        word1: 'longa',
+        source1: 'Audio/Irish/Carraroe/longa.ogg',
+        sound2: '/ɲ/',
+        word2: 'loinge',
+        source2: 'Audio/Irish/Carraroe/loinge.ogg'
+    }
 
-const broadRSlenderR = {
-  sound1: '/ɾˠ/',
-  word1: 'cur',
-  source1: 'Carraroe/cur.ogg',
-  sound2: '/ɾʲ/',
-  word2: 'cuir',
-  source2: 'Carraroe/cuir.ogg'
-}
+    const broadRSlenderR = {
+        sound1: '/ɾˠ/',
+        word1: 'cur',
+        source1: 'Audio/Irish/Carraroe/cur.ogg',
+        sound2: '/ɾʲ/',
+        word2: 'cuir',
+        source2: 'Audio/Irish/Carraroe/cuir.ogg'
+    }
 
-const broadLSlenderL = {
-  sound1: '/l̪ˠ/',
-  word1: 'lón',
-  source1: 'Carraroe/lon.ogg',
-  sound2: '/l̠ʲ/',
-  word2: 'leon',
-  source2: 'Carraroe/leon.ogg'
-}
+    const broadLSlenderL = {
+        sound1: '/l̪ˠ/',
+        word1: 'lón',
+        source1: 'Audio/Irish/Carraroe/lon.ogg',
+        sound2: '/l̠ʲ/',
+        word2: 'leon',
+        source2: 'Audio/Irish/Carraroe/leon.ogg'
+    }
 
-const broadPBroadB = {
-  sound1: '/pˠ/',
-  word1: 'pá',
-  source1: 'Carraroe/pa.ogg',
-  sound2: '/bˠ/',
-  word2: 'bá',
-  source2: 'Carraroe/ba.ogg'
-}
+    const broadPBroadB = {
+        sound1: '/pˠ/',
+        word1: 'pá',
+        source1: 'Audio/Irish/Carraroe/pa.ogg',
+        sound2: '/bˠ/',
+        word2: 'bá',
+        source2: 'Audio/Irish/Carraroe/ba.ogg'
+    }
 
-const slenderPSlenderB = {
-  sound1: '/pʲ/',
-  word1: 'pinn',
-  source1: "Carraroe/pinn.ogg",
-  sound2: '/bʲ/',
-  word2: 'binn',
-  source2: "Carraroe/binn.ogg"
-}
+    const slenderPSlenderB = {
+        sound1: '/pʲ/',
+        word1: 'pinn',
+        source1: "Audio/Irish/Carraroe/pinn.ogg",
+        sound2: '/bʲ/',
+        word2: 'binn',
+        source2: "Audio/Irish/Carraroe/binn.ogg"
+    }
 
-const broadTBroadD = {
-  sound1: '/t̪ˠ/',
-  word1: 'ghreadfadh',
-  source1: 'Carraroe/ghreadfadh.ogg',
-  sound2: '/d̪ˠ/',
-  word2: 'ghreadadh',
-  source2: 'Carraroe/ghreadadh.ogg'
-}
+    const broadTBroadD = {
+        sound1: '/t̪ˠ/',
+        word1: 'ghreadfadh',
+        source1: 'Audio/Irish/Carraroe/ghreadfadh.ogg',
+        sound2: '/d̪ˠ/',
+        word2: 'ghreadadh',
+        source2: 'Audio/Irish/Carraroe/ghreadadh.ogg'
+    }
 
-const slenderTSlenderD = {
-  sound1: '/t̠ʲ/',
-  word1: 'chreidfeadh',
-  source1: 'Carraroe/chreidfeadh.ogg',
-  sound2: '/d̠ʲ/',
-  word2: 'chreideadh',
-  source2: 'Carraroe/chreideadh.ogg'
-}
+    const slenderTSlenderD = {
+        sound1: '/t̠ʲ/',
+        word1: 'chreidfeadh',
+        source1: 'Audio/Irish/Carraroe/chreidfeadh.ogg',
+        sound2: '/d̠ʲ/',
+        word2: 'chreideadh',
+        source2: 'Audio/Irish/Carraroe/chreideadh.ogg'
+    }
 
-const broadCBroadG = {
-  sound1: '/k/',
-  word1: 'cá',
-  source1: 'Carraroe/cá.ogg',
-  sound2: '/g/',
-  word2: 'gá',
-  source2: 'Carraroe/gá.ogg'
-}
+    const broadCBroadG = {
+        sound1: '/k/',
+        word1: 'cá',
+        source1: 'Audio/Irish/Carraroe/cá.ogg',
+        sound2: '/g/',
+        word2: 'gá',
+        source2: 'Audio/Irish/Carraroe/gá.ogg'
+    }
 
-const slenderCSlenderG = {
-  sound1: '/c/',
-  word1: 'ciall',
-  source1: 'Carraroe/ciall.ogg',
-  sound2: '/ɟ/',
-  word2: 'giall',
-  source2: 'Carraroe/giall.ogg'
-}
+    const slenderCSlenderG = {
+        sound1: '/c/',
+        word1: 'ciall',
+        source1: 'Audio/Irish/Carraroe/ciall.ogg',
+        sound2: '/ɟ/',
+        word2: 'giall',
+        source2: 'Audio/Irish/Carraroe/giall.ogg'
+    }
 
 
-const broadFandW = {
-  sound1: '/fˠ/',
-  word1: '(faoi)',
-  source1: 'Carraroe/faoi.ogg',
-  sound2: '/w/',
-  word2: '(bhuí)',
-  source2: 'Carraroe/bhuí.ogg'
-}
+    const broadFandW = {
+        sound1: '/fˠ/',
+        word1: '(faoi)',
+        source1: 'Audio/Irish/Carraroe/faoi.ogg',
+        sound2: '/w/',
+        word2: '(bhuí)',
+        source2: 'Audio/Irish/Carraroe/bhuí.ogg'
+    }
 
-const slenderFandV = {
-  sound1: "/fʲ/",
-  word1:  "(figh)",
-  source1: "Carraroe/figh.ogg",
-  sound2: "/vʲ/",
-  word2: '(bhí)',
-  source2: 'Carraroe/bhí.ogg'
-}
+    const slenderFandV = {
+        sound1: "/fʲ/",
+        word1: "(figh)",
+        source1: "Audio/Irish/Carraroe/figh.ogg",
+        sound2: "/vʲ/",
+        word2: '(bhí)',
+        source2: 'Audio/Irish/Carraroe/bhí.ogg'
+    }
 
-const broadChBroadGh = {
-  sound1: '/x/',
-  word1: 'an-chaol',
-  source1: 'Carraroe/an-chaol.ogg',
-  sound2: '/ɣ/',
-  word2: 'an-ghaol',
-  source2: 'Carraroe/an-ghaol.ogg'
-}
+    const broadChBroadGh = {
+        sound1: '/x/',
+        word1: 'an-chaol',
+        source1: 'Audio/Irish/Carraroe/an-chaol.ogg',
+        sound2: '/ɣ/',
+        word2: 'an-ghaol',
+        source2: 'Audio/Irish/Carraroe/an-ghaol.ogg'
+    }
 
-const slenderChSlenderGh = {
-  sound1: '/ç/',
-  word1: 'cheal',
-  source1: 'Carraroe/cheal.ogg',
-  sound2: '/ʝ/',
-  word2: 'gheal',
-  source2: 'Carraroe/gheal.ogg'
-}
+    const slenderChSlenderGh = {
+        sound1: '/ç/',
+        word1: 'cheal',
+        source1: 'Audio/Irish/Carraroe/cheal.ogg',
+        sound2: '/ʝ/',
+        word2: 'gheal',
+        source2: 'Audio/Irish/Carraroe/gheal.ogg'
+    }
 
-const carraroeSounds=[broadPSlenderP, broadBSlenderB, broadTSlenderT, broadDSlenderD,
-broadCSlenderC,broadGSlenderG,broadFSlenderF,broadBhSlenderBh,broadSSlenderS,broadChSlenderCh,
-broadGhSlenderGh,broadMSlenderM,broadNSlenderN,broadNgSlenderNg,broadRSlenderR,broadLSlenderL,broadPBroadB,
-slenderPSlenderB,broadTBroadD,slenderTSlenderD,broadCBroadG,slenderCSlenderG,broadFandW,slenderFandV,broadChBroadGh,slenderChSlenderGh]
-
-carraroeSounds.forEach(addTableRows);
-function practice(){
-  test(carraroeSounds)
-}
+    return [broadPSlenderP, broadBSlenderB, broadTSlenderT, broadDSlenderD,
+        broadCSlenderC, broadGSlenderG, broadFSlenderF, broadBhSlenderBh, broadSSlenderS, broadChSlenderCh,
+        broadGhSlenderGh, broadMSlenderM, broadNSlenderN, broadNgSlenderNg, broadRSlenderR, broadLSlenderL, broadPBroadB,
+        slenderPSlenderB, broadTBroadD, slenderTSlenderD, broadCBroadG, slenderCSlenderG, broadFandW, slenderFandV, broadChBroadGh, slenderChSlenderGh
+    ]
 }());
--- Javascript/script.js
+++ Javascript/script.js
@@ -1,85 +1,93 @@
 const table = document.getElementById("table");
+let sounds;
 
-function loadJS(element){
-  toggleHide(element.target.value);
-  removeTableRows();
-  if(element.target.value){
-  let newScript;
-  try {document.body.removeChild(document.getElementById('lib_lang'));} catch(e){}
-  newScript  = document.createElement('script');
-  newScript.id = 'lib_lang';
-  newScript.type = 'text/javascript';
-  newScript.src = 'Javascript/'+element.target.value+'.js';
-  document.body.appendChild(newScript);
-}
+function loadJS(element) {
+    document.getElementById('answer').innerHTML = '';
+    toggleHide('')
+    removeTableRows();
+    if (element.target.value) {
+        let newScript;
+        try {
+            document.body.removeChild(document.getElementById('lib_lang'));
+        } catch (e) {}
+        newScript = document.createElement('script');
+        newScript.async = 1;
+        newScript.id = 'lib_lang';
+        newScript.type = 'text/javascript';
+        newScript.src = 'Javascript/' + element.target.value + '.js';
+        newScript.onload = function() {
+            toggleHide(element.target.value);
+            sounds.forEach(addTableRows)
+            sounds = sounds;
+        }
+        document.body.appendChild(newScript);
+    }
 }
 
-function addTableRows(element){
-  const row = table.insertRow(-1);
-  const cell1 = row.insertCell(0);
-  const cell2 = row.insertCell(1);
-  cell1.innerHTML = `${element.sound1} (${element.word1}) <audio id='${element.word1.split(' ').join('')}'
+function addTableRows(element) {
+    const row = table.insertRow(-1);
+    const cell1 = row.insertCell(0);
+    const cell2 = row.insertCell(1);
+    cell1.innerHTML = `${element.sound1} (${element.word1}) <audio id='${element.word1.split(' ').join('')}'
   src=${element.source1}></audio><button onclick=document.getElementById('${element.word1.split(' ').join('')}').play()>&#9658;</button>`;
-  cell2.innerHTML = `${element.sound2} (${element.word2}) <audio id='${element.word2.split(' ').join('')}'
+    cell2.innerHTML = `${element.sound2} (${element.word2}) <audio id='${element.word2.split(' ').join('')}'
   src=${element.source2}></audio><button onclick=document.getElementById('${element.word2.split(' ').join('')}').play()>&#9658;</button>`;
 }
 
-function removeTableRows(){
-  count = table.getElementsByTagName('tr').length;
-  for(let x=count-1; x>0; x--){
-    table.deleteRow(x);
-  }
+function removeTableRows() {
+    count = table.getElementsByTagName('tr').length;
+    for (let x = count - 1; x > 0; x--) {
+        table.deleteRow(x);
+    }
 }
 
-function test(language){
-  document.getElementById('correct').innerHTML= '';
-  const newEl = document.createElement('div');
-  const random1 = rand(carraroeSounds.length-1);
-  const random2 = rand(2);
-  let source = "source"+random2;
-  let word = "word"+random2;
-  source = language[random1][source];
-  word = language[random1][word];
-  if(word.charAt(0)==='('){
-    word=word.substring(1,word.length-1);
-  }
-  const div = document.getElementById('answer');
-  const HTML1 = `<audio id='${word.split(' ').join('')}' src=${source}></audio>
+function test() {
+    document.getElementById('correct').innerHTML = '';
+    const newEl = document.createElement('div');
+    const random1 = rand(0, sounds.length);
+    const random2 = rand(1, 3);
+    let source = "source" + random2;
+    let word = "word" + random2;
+    source = sounds[random1][source];
+    word = sounds[random1][word];
+    if (word.charAt(0) === '(') {
+        word = word.substring(1, word.length - 1);
+    }
+    const div = document.getElementById('answer');
+    const HTML1 = `<audio id='${word.split(' ').join('')}' src=${source}></audio>
     <button onclick=document.getElementById('${word.split(' ').join('')}').play()>&#9658;</button>`;
-  const HTML2 =  `<input type="text" id="solution" name="answer" placeholder="Your answer here"/>
+    const HTML2 = `<input type="text" id="solution" name="answer" placeholder="Your answer here"/>
     <button onclick="check('${word}')">Check</button>`;
-  div.innerHTML = HTML1 + HTML2;
+    div.innerHTML = HTML1 + HTML2;
 }
 
-function check(word){
-  const div = document.getElementById('correct');
-  div.classList.remove('incorrect', 'correct');
-  const solution = document.getElementById('solution').value.toLowerCase();
-  if(solution===word){
-    div.innerHTML = "Congratulations, you are correct! Hit test to get a different word"
-    div.classList.add('correct');
-  }
-  else{
-    div.innerHTML = `Sorry, the word was ${word}. Hit test to try a new word.`;
-    div.classList.add('incorrect');
-  }
+function check(word) {
+    const div = document.getElementById('correct');
+    div.classList.remove('incorrect', 'correct');
+    const solution = document.getElementById('solution').value.toLowerCase();
+    if (solution === word) {
+        div.innerHTML = "Congratulations, you are correct! Hit test to get a different word"
+        div.classList.add('correct');
+    } else {
+        div.innerHTML = `Sorry, the word was ${word}. Hit test to try a new word.`;
+        div.classList.add('incorrect');
+    }
 }
 
-function toggleHide(value){
-  let elements = document.getElementsByClassName('hidden');
-  if(value){
-    elements[0].style.display = '';
-    elements[1].style.display = '';
-  }
-  else{
-    elements[0].style.display = 'none';
-    elements[1].style.display = 'none'
-  }
+function toggleHide(value) {
+    let elements = document.getElementsByClassName('hidden');
+    if (value) {
+        elements[0].style.display = '';
+        elements[1].style.display = '';
+    } else {
+        elements[0].style.display = 'none';
+        elements[1].style.display = 'none'
+    }
 }
 
-function rand(upperBound){
-  return Math.floor((Math.random()*upperBound)+1);
+function rand(lowerBound, upperBound) {
+    return Math.floor(Math.random() * (upperBound - lowerBound)) + lowerBound;
 }
 
 toggleHide()
-document.getElementById('minimal').addEventListener('change',loadJS);
+document.getElementById('minimal').addEventListener('change', loadJS);
